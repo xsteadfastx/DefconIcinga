0c3b413892250033fe654295bb9ddbde67a6ebd4
Marvin Steadfast <xsteadfastx@gmail.com>
2014-03-23 12:05:03 +0100

    rewrite with flask-socketio

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 DefconIcinga
 ============
+
+a simple webapp showing the "defcon level" of the network monitored by [icinga](https://www.icinga.org/). it changes the background color for each level. 
+
+## Based on ##
+- flask
+- flask-socketio
+- socketio
+- requests
+- ... 
--- defcon_icinga.py
+++ defcon_icinga.py
@@ -1,19 +1,30 @@
+import os
+import time
 import requests
-from flask import Flask, render_template
+import json
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_bootstrap import Bootstrap
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room
+
+
+HOST = '127.0.0.1'
+AUTHKEY = 'xyz'
+URL = 'http://%s/icinga-web/web/api/service/filter/columns[SERVICE_NAME|HOST_NAME|SERVICE_CURRENT_STATE]/authkey=%s/json' % (HOST, AUTHKEY)
 
 
 app = Flask(__name__)
-
-
-red = '#911821'
-yellow = '#c3a41e'
-green = '#3aad2f'
+app.debug = True
+app.config['SECRET_KEY'] = os.urandom(24)
+Bootstrap(app)
+socketio = SocketIO(app)
 
 
 def defcon():
-    HOST = '127.0.0.1'
-    AUTHKEY = 'xyzxyzxyz'
-    URL = 'http://%s/icinga-web/web/api/service/filter/columns[SERVICE_NAME|HOST_NAME|SERVICE_CURRENT_STATE]/authkey=%s/json' % (HOST, AUTHKEY)
+    colors = {'red': '#d9534f',
+              'yellow': '#f0ad4e',
+              'green': '#5cb85c'}
+
     r = requests.get(URL)
     problems = []
     for i in r.json()['result']:
@@ -21,18 +32,41 @@ def defcon():
             problems.append(i)
 
     if len(problems) == 0:
-        color = green
+        color = colors['green']
     elif 1 <= len(problems) <= 3:
-        color = yellow
+        color = colors['yellow']
     else:
-        color = red
+        color = colors['red']
 
-    return color, len(problems), problems
+    return json.dumps({'check_time': time.strftime('%H:%M:%S'), 'color': color, 'count': len(problems), 'problems':
+                      problems})
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    while True:
+        time.sleep(10)
+        defcon_json = defcon()
+        socketio.emit('my response', defcon_json, namespace='/defcon')
 
 
 @app.route('/')
 def index():
-    problems = defcon()
-    background_color = problems[0]
-    return render_template('index.html', background_color = background_color,
-                           problems_len = problems[1], problems = problems[2])
+    return render_template('index.html')
+
+
+@socketio.on('connect', namespace='/defcon')
+def defcon_connect():
+    print('Client connected')
+    defcon_json = defcon()
+    emit('my response', defcon_json, namespace='/defcon')
+
+
+@socketio.on('disconnect', namespace='/defcon')
+def defcon_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    Thread(target=background_thread).start()
+    socketio.run(app)
